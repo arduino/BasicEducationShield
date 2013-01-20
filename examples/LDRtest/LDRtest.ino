1305d4418704ed1b5ed3bf4d0ce26eab7df91bcc
Clara <clara.leivas@gmail.com>
2013-01-20 16:31:26 +0100

    Changed LDRtest.ino

--- examples/LDRtest/LDRtest.ino
+++ examples/LDRtest/LDRtest.ino
@@ -1,17 +1,15 @@
+/*
+
+*/
 #include <Castilla.h>
 
-LDR me=LDR(A1);
+LDR ldr = LDR(A1);
 
 void setup(){
   Serial.begin(9600);
-  me.config(730,600);
-  delay(1000);
 }
 
 void loop(){
-  //me.test();
-  //delay(30);
-
-  Serial.println("Please press ldr...");
-  Serial.println(me.pressed());
+  ldr.test();
+  delay(100);
 }
