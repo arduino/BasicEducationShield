bf5472311c5fb79291592e7ab4ad3c2f847eb7ff
X-Y <xunyang.sve@gmail.com>
2013-01-23 19:46:25 +0100

    Update examples/Sports/racing/racing.ino

--- examples/Sports/racing/racing.ino
+++ examples/Sports/racing/racing.ino
@@ -1,8 +1,26 @@
+/*
+Racing
+
+Run as fast as you can in this game. Tap two sensors
+with your hands repeatedly, and you can see the progress
+through the scoreboard made of a line of LEDs. When you
+finally made it to the end, all LEDs on the scoreboard
+will start blinking for your victory. 
+*/
 #include <CapacitiveSensor.h>
 #include <Castilla.h>
 
+/*
+An array of pin numbers to which LEDs are attached
+the defaults are 2 to 6 but you can choose any of the digital pins
+just remember to leave digital pin 9 and 10 for the buttons
+*/
+int ledPins[] = {2, 3, 4, 5, 6};  
+int pinCount = 5;
 VUMeter scoreBoard;
 
+//Configure the capacitive sensors
+int capacitiveSensorThreshold=900;
 CapacitiveSwitch leftFoot=CapacitiveSwitch(4,3);
 CapacitiveSwitch rightFoot=CapacitiveSwitch(4,2);
 
@@ -12,36 +30,32 @@ boolean win;
 void setup(){
   //initializing the game, set up all the components
   //and variables
-  Serial.begin(9600);
   score=0;
   win=false;
   
   //Connect scoreboard
-  int pins[]={9,10,11,12,13};
-  scoreBoard.config(5,pins);
+  scoreBoard.config(pinCount,ledPins);
   scoreBoard.initialize();
   
   //initialize left and right foot
-  leftFoot.config(900);
-  rightFoot.config(900);
+  leftFoot.config(capacitiveSensorThreshold);
+  rightFoot.config(capacitiveSensorThreshold);
 }
 void loop(){
-  if(!win){
-    //Wait for the left foot to be pressed
-    leftFoot.pressed();
-    //Wait for the right foot to be pressed
-    rightFoot.pressed();
-    //if both feet are pressed, add one point
-    score=score+1;
-    Serial.println(score);
-    //When you get 100 points, you win
-    if(score>100){
-      win=true;
-    }
-    //Every 20 points light up a led
-    scoreBoard.fill(score/20);
-  }else{
-    //if you win, blink all leds for celebration
+  //Wait for the left foot to be pressed
+  leftFoot.pressed();
+  //Wait for the right foot to be pressed
+  rightFoot.pressed();
+  //After both feet are pressed, add one point
+  score=score+1;
+  //When you get 100 points, you win
+  if(score>100){
+    win=true;
+  }
+  //Every 20 points light up a led
+  scoreBoard.fill(score/20);
+  //if you win, blink all leds for celebration
+  if(win){
     scoreBoard.blinkAll(100,4);
     //and reset the game
     win=false;
