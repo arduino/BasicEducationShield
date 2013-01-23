e0e65fe0af5f68cfd785984534f13ef5d48d389b
X-Y <xunyang.sve@gmail.com>
2013-01-23 19:35:50 +0100

    Update examples/Sports/pong/pong.ino

--- examples/Sports/pong/pong.ino
+++ examples/Sports/pong/pong.ino
@@ -1,8 +1,13 @@
 /*
-Description
+Pong
 
+This is a small pong game consisting of a row of 5 LEDs(VU-meter)
+and one button on each side of the row. Play it with a friend, both
+of you should have a button in hand. When the game starts, the 
+“pong” will fly towards you. Press your button to hit the pong when
+it reaches the end, so it bounces to your friend. If either of you
+fail to hit “pong” in time, the other player will be the winner. 
 */
-
 
 #include <Castilla.h>
 /*
@@ -14,8 +19,8 @@ int ledPins[] = {2, 3, 4, 5, 6};
 int pinCount = 5;
 VUMeter vuMeter; 
 
-Button button1 = Button(9, HIGH); //the button connected to digital pin 9
-Button button2 = Button(10, HIGH); //the button connected to digital pin 10
+Button button1 = Button(9); //the button connected to digital pin 9
+Button button2 = Button(10); //the button connected to digital pin 10
 
 int ledTime = 100; //determines how fast the LEDs will switch
 int pressTime = 200; //determines how long time a player has to press the button
