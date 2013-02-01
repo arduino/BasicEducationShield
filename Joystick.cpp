951279a671b24c4529fb36290b54592cc315b9eb
Clara <clara.leivas@gmail.com>
2013-02-01 16:42:25 +0100

    Added Joystick to library

---
+++ Joystick.cpp
@@ -0,0 +1,20 @@
+#include "Castilla.h"
+
+Joystick::Joystick(int x, int y){
+	this->x=x;
+	this->y=y;
+}
+
+int Joystick::getX(){
+    int v = analogRead(x);
+    if(v<300) return(1);
+    else if(v>700) return(2);
+    else return(0);
+
+}
+int Joystick::getY(){
+    int v = analogRead(y);
+    if(v<300) return(1);
+    else if(v>700) return(2);
+    else return(0);
+}
