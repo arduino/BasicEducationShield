8ee3a589a4b362c17d57201705ea8a48d3b5633a
Clara <clara.leivas@gmail.com>
2013-02-01 16:41:15 +0100

    Added Joystick example under help

---
+++ examples/help/Joystick/Joystick.ino
@@ -0,0 +1,16 @@
+
+#include <Castilla.h>
+
+//The joystick uses two analog pins. Here pin A0 and A1 is used
+Joystick me=Joystick(A0,A1);
+
+void setup(){
+  Serial.begin(9600);
+}
+void loop(){
+  Serial.print("X: ");  
+  Serial.print(me.getX()); //Print the X value. 0 is middle
+  Serial.print("  Y: ");
+  Serial.println(me.getY()); //Print the Y value. 0 is middle
+  delay(200);
+}
