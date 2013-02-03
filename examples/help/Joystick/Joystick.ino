14690edcf44c702c710dfb1d2db34759c3b15f5b
Clara <clara.leivas@gmail.com>
2013-02-03 15:18:36 +0100

    Made small change in Joystick.ino

--- examples/help/Joystick/Joystick.ino
+++ examples/help/Joystick/Joystick.ino
@@ -2,7 +2,7 @@
 #include <Castilla.h>
 
 //The joystick uses two analog pins. Here pin A0 and A1 is used
-Joystick me=Joystick(A0,A1);
+Joystick me=Joystick(A1,A0);
 
 void setup(){
   Serial.begin(9600);
