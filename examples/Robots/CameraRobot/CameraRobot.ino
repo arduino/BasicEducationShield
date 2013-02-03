fa048573d8ad31ada6c6ac75d0d5dc3edb5b28fb
Clara <clara.leivas@gmail.com>
2013-02-03 11:54:16 +0100

    Updated some lines in code

--- examples/Robots/CameraRobot/CameraRobot.ino
+++ examples/Robots/CameraRobot/CameraRobot.ino
@@ -12,29 +12,33 @@ LimitedServo tilt = LimitedServo(10); //Servo that tilts
 int panAngle = 90; //Pan angle
 int tiltAngle = 90; //Tilt angle
 
+int panJoystick = 0;
+int tiltJoystick = 0;
+
 void setup(){
-  Serial.begin(9600);
   pan.begin(); //servo is initialized
   tilt.begin(); //servo is initialized
 }
 void loop(){
-  if(joystick.getX()==1 && panAngle<180){
+  panJoystick = joystick.getX();
+  tiltJoystick = joystick.getY();
+  if(panJoystick==1 && panAngle<180){
     //If the X value from the joystick equals 1 and
     //panAngle is less than 180 degrees, increase panAngle with 1
     panAngle++;
   }
-  else if(joystick.getX()==2 && panAngle>0){
+  else if(panJoystick==2 && panAngle>0){
     //If the X value from the joystick equals 2 and
     //panAngle is more than 0 degrees, decrease panAngle with 1
     panAngle--;
   }
   
-  if(joystick.getY()==1 && tiltAngle<180){ 
+  if(tiltJoystick==1 && tiltAngle<180){ 
     //If the Y value from the joystick equals 1 and
     //tiltAngle is less than 180 degrees, increase tiltAngle with 1
     tiltAngle++; 
   }
-  else if(joystick.getY()==2 && tiltAngle>0){ 
+  else if(tiltJoystick==2 && tiltAngle>0){ 
     //If the Y value from the joystick equals 2 and
     //tiltAngle is more than 0 degrees, decrease tiltAngle with 1
     tiltAngle--;
