4775cb9516b30b18996dbbc366906790f9a491ad
Clara <clara.leivas@gmail.com>
2013-02-03 17:08:56 +0100

    Made small change with returning values in library

--- Joystick.cpp
+++ Joystick.cpp
@@ -7,14 +7,14 @@ Joystick::Joystick(int x, int y){
 
 int Joystick::getX(){
     int v = analogRead(x);
-    if(v<300) return(1);
-    else if(v>700) return(2);
+    if(v<300) return(-1);
+    else if(v>700) return(1);
     else return(0);
 
 }
 int Joystick::getY(){
     int v = analogRead(y);
-    if(v<300) return(1);
-    else if(v>700) return(2);
+    if(v<300) return(-1);
+    else if(v>700) return(1);
     else return(0);
 }
