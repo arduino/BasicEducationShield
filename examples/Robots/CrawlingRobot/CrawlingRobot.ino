91770eeefffc8ee4b7efcd8aae747568093f1098
Clara <clara.leivas@gmail.com>
2013-02-07 15:05:32 +0100

    Made some changes to make crawling robot crawl better

--- examples/Robots/CrawlingRobot/CrawlingRobot.ino
+++ examples/Robots/CrawlingRobot/CrawlingRobot.ino
@@ -3,8 +3,8 @@
 
 // Two standard servos are connected to each of 
 // the tinkerkit digital connections, that is 9 and 10
-LimitedServo front=LimitedServo(9); 
-LimitedServo back=LimitedServo(10);
+StandardServo front=StandardServo(9); 
+StandardServo back=StandardServo(10);
 
 void setup(){
   front.begin(); //servos are initialized
@@ -15,9 +15,11 @@ void loop(){
   //Make the robot crawl by setting the servos to opposite angles.
   //90 is the middle angle
   front.setAngle(120);
+  delay(200);
   back.setAngle(60);
-  delay(500);
+  delay(200);
   front.setAngle(60);
+  delay(200);
   back.setAngle(120);
-  delay(500);
+  delay(200);
 }
