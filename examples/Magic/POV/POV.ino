7f446c4676c7b1461c5202fb0e1296432911056b
Torsten Hansson <pseudoev@gmail.com>
2013-02-07 19:11:58 +0100

    POV Example (I UZ GITHUB CLIENT LAL)
    POV is Persistence of Vision, yo

---
+++ examples/Magic/POV/POV.ino
@@ -0,0 +1,61 @@
+/*
+POV (Persistence of Vision)
+
+This is a toy consisting of a row of 5 LEDs(VU-meter).
+It is a proof of concept of the illusion of a text display
+using little effort. The user will have to wave the row of LEDs
+quickly back and forth to achieve the effect.
+The button helps change settings for the display of this effect.
+*/
+
+#include <Castilla.h>
+/*
+An array of pin numbers to which LEDs are attached
+the defaults are 2 to 6 but you can choose any of the digital pins
+*/
+int ledPins[] = {2, 3, 4, 5, 6};
+int pinCount = 5;
+VUMeter vuMeter;
+
+int rowCount = 0; // Stores the number of rows
+int rowLength = 22; // Width of the message, copy this number the message array
+int delayTime = 5; // Time it takes to show a row in milliseconds
+
+// The message where 0 is LOW and 1 is HIGH
+int message[5][22]={
+  // H H H H   O O O O   L L L L   A A A A
+  {0,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,1,1,0,0,0},
+  {0,1,0,0,1,0,1,0,0,1,0,1,0,0,0,0,1,0,0,1,0,0},
+  {0,1,1,1,1,0,1,0,0,1,0,1,0,0,0,0,1,1,1,1,0,0},
+  {0,1,0,0,1,0,1,0,0,1,0,1,0,0,0,0,1,0,0,1,0,0},
+  {0,1,0,0,1,0,0,1,1,0,0,1,1,1,1,0,1,0,0,1,0,0}
+};
+
+void setup(){
+  // If your are using other pins than 2 to 6 you need to configure that here
+  vuMeter.config(pinCount, ledPins);
+  
+  vuMeter.begin(); //does the same as pinMode, LEDs are outputs
+}
+
+void loop(){
+ // If the whole array has been drawn
+  if(rowCount == rowLength) {
+    rowCount = 0; // Reset the rowCount
+  } else {
+    // Shows the message
+    for (int i = 2; i < 7; i++) {
+      // Checks if the array says HIGH
+      if (message[i-1][rowCount] == 1) {
+        vuMeter.ON(i);
+      } else {
+        vuMeter.OFF(i);
+      }
+    }
+    rowCount++;
+  }
+
+  delay(delayTime); // This is the delay per row
+
+}
+
