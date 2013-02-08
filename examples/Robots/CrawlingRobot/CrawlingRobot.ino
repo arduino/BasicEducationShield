6eef631c472fb6a9d3ebbe4d6c2ec0945f9aad7b
Clara <clara.leivas@gmail.com>
2013-02-08 22:29:24 +0100

    Added detach/attach

--- examples/Robots/CrawlingRobot/CrawlingRobot.ino
+++ examples/Robots/CrawlingRobot/CrawlingRobot.ino
@@ -12,12 +12,23 @@ void setup(){
 void loop(){
   //Make the robot crawl by setting the servos to opposite angles.
   //90 is the middle angle
-  front.write(120);
+  back.detach();
+  front.attach(9);
+  front.write(120);   //Rotation front legs
   delay(200);
+  
+  front.detach();
+  back.attach(10);
   back.write(60);
   delay(200);
+  
+  back.detach();
+  front.attach(9);
   front.write(60);
   delay(200);
+  
+  front.detach();
+  back.attach(10);
   back.write(120);
   delay(200);
 }
