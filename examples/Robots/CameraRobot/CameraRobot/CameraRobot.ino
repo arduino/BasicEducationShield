5eae818d5487e226da2cfe5fd44005c1c1d0a9df
Clara <clara.leivas@gmail.com>
2013-02-08 10:58:16 +0100

    Updated according to library changes

--- examples/Robots/CameraRobot/CameraRobot/CameraRobot.ino
+++ examples/Robots/CameraRobot/CameraRobot/CameraRobot.ino
@@ -2,7 +2,7 @@
 #include <Castilla.h>
 #include <Servo.h>
 
-//The joystick uses two analog pins. Here pin A0 and A1 is used
+//The joystick uses two analog pins. Here pin A1 and A0 is used
 
 Joystick joystick = Joystick(A1,A0);//Joystick(XanalogPin, YanalogPin)
 
@@ -16,31 +16,32 @@ int panJoystick = 0;
 int tiltJoystick = 0;
 
 void setup(){
+  Serial.begin(9600);
   pan.begin(); //servo is initialized
   tilt.begin(); //servo is initialized
 }
 void loop(){
-  panJoystick = joystick.getX(); //Get X value from joystick
-  tiltJoystick = joystick.getY(); //Get Y value from joystick
-  
-  if(panJoystick==1 && panAngle<180){
-    //If the X value from the joystick equals 1 and
+  panJoystick = joystick.getX();
+  tiltJoystick = joystick.getY();
+  Serial.println(joystick.getX());
+  if(panJoystick==-1 && panAngle<180){
+    //If the X value from the joystick equals -1 and
     //panAngle is less than 180 degrees, increase panAngle with 1
     panAngle++;
   }
-  else if(panJoystick==-1 && panAngle>0){
-    //If the X value from the joystick equals -1 and
+  else if(panJoystick==1 && panAngle>0){
+    //If the X value from the joystick equals 1 and
     //panAngle is more than 0 degrees, decrease panAngle with 1
     panAngle--;
   }
   
-  if(tiltJoystick==1 && tiltAngle<180){ 
-    //If the Y value from the joystick equals 1 and
+  if(tiltJoystick==-1 && tiltAngle<180){ 
+    //If the Y value from the joystick equals -1 and
     //tiltAngle is less than 180 degrees, increase tiltAngle with 1
     tiltAngle++; 
   }
-  else if(tiltJoystick==-1 && tiltAngle>0){ 
-    //If the Y value from the joystick equals -1 and
+  else if(tiltJoystick==1 && tiltAngle>0){ 
+    //If the Y value from the joystick equals 1 and
     //tiltAngle is more than 0 degrees, decrease tiltAngle with 1
     tiltAngle--;
   }
