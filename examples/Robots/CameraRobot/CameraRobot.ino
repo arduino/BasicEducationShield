b3ae0ee823759ff19c4fbae9949c7140029660ed
piratecheese <clara.leivas@gmail.com>
2014-01-02 16:49:44 +0100

    Updated Camera robot

--- examples/Robots/CameraRobot/CameraRobot.ino
+++ examples/Robots/CameraRobot/CameraRobot.ino
@@ -11,7 +11,6 @@
 #include <Servo.h>
 
 //The joystick uses two analog pins. Here pin A0 and A1 is used
-
 Joystick joystick = Joystick(A1,A0);//Joystick(XanalogPin, YanalogPin)
 
 Servo pan, tilt;
@@ -33,27 +32,23 @@ void loop(){
 
   //If the joysticks x value isn't 0 we will pan
   if(panJoystick!=0){
-    tilt.detach();
-    pan.attach(9);
     //If the X value from the joystick equals 1 and
     //panAngle is less than 180 degrees, increase panAngle with 1
     if(panJoystick==1 && panAngle<180) panAngle++;
     //If the X value from the joystick equals -1 and
     //panAngle is more than 0 degrees, decrease panAngle with 1
     else if(panJoystick==-1 && panAngle>0) panAngle--;
-    pan.write(panAngle); //Set position of the pan servo
   }
-  //If the joysticks x value is 0 we will tilt
-  else{
-    pan.detach();
-    tilt.attach(10);
+  //If the joysYicks x value is 0 we will tilt
+  if(tiltJoystick!=0){
     //If the Y value from the joystick equals 1 and
     //tiltAngle is less than 180 degrees, increase tiltAngle with 1
     if(tiltJoystick==1 && tiltAngle<180) tiltAngle++;
     //If the Y value from the joystick equals -1 and
     //tiltAngle is more than 0 degrees, decrease tiltAngle with 1
     else if(tiltJoystick==-1 && tiltAngle>0) tiltAngle--;
-    tilt.write(tiltAngle); //Set position of the tilt servo
   }
+  pan.write(panAngle); //Set position of the pan servo
+  tilt.write(tiltAngle); //Set position of the tilt servo
   delay(5);
 }
